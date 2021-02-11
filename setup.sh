25eed5ef2abc5df64f222ec2d32925a3c75fb833
Jay Cuevas <Jordan.Cuevas@precisecal.com>
2021-02-11 16:57:19 -0500

    Add j4 dmenu and fonts setup

--- setup.sh
+++ setup.sh
@@ -3,18 +3,26 @@ baseDir=$HOME/repos
 root=$(pwd)
 
 # Install dependencies
-sudo apt install -y xorg-dev xcb libxcb-xkb-dev x11-xkb-utils libx11-xcb-dev libxkbcommon-x11-dev libxcb-res0-dev picom
+sudo apt install -y xorg-dev xcb libxcb-xkb-dev x11-xkb-utils libx11-xcb-dev libxkbcommon-x11-dev libxcb-res0-dev picom cmake
 
 mkdir -p $baseDir
 git clone $gh/dwm.git $baseDir/dwm
 git clone $gh/st.git $baseDir/st
+git clone $gh/dmenu.git $baseDir/dmenu
+git clone https://github.com/enkore/j4-dmenu-desktop.git $baseDir/j4dmenu
 
 # Install dwm and st
 cd $baseDir/dwm && sudo make clean install
 cd $baseDir/st && sudo make clean install
+cd $baseDir/j4dmenu && cmake . && make && sudo make install
 
 echo "W0Rlc2t0b3AgRW50cnldClZlcnNpb249MS4wCk5hbWU9RHdtIFNlc3Npb24KRXhlYz1kd20KSWNvbj0KVHlwZT1BcHBsaWNhdGlvbgpEZXNrdG9wTmFtZXM9RFdNCg==" | base64 -d | sudo tee /usr/share/xsessions/dwm.desktop
 
-
 mkdir -p $HOME/.config/picom
 cp $root/picom.conf $HOME/.config/picom
+
+git clone https://github.com/ryanoasis/nerd-fonts --depth 1 $baseDir/nerdfonts
+cd $baseDir/nerdfonts
+
+./install.sh FiraCode
+./install.sh Mononoki
